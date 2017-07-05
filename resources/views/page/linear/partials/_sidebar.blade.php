d0bed073de69ca6390324615ebc7127aab5c251d
scslingsby <scslingsby@gmail.com>
2017-07-05 13:29:26 +0100

    Rearrange sidebar items

--- resources/views/page/linear/partials/_sidebar.blade.php
+++ resources/views/page/linear/partials/_sidebar.blade.php
@@ -4,14 +4,14 @@
 <ul>
     @foreach($pages as $page)
         <li>
+            @if($page->current)
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            @endif
             <a href="{{ url('a/' . $activity->id . '/student/r/' . $pageData->roundNumber . '/p/' . $page->pageNumber) }}">
                 {{ $page->title }}
             </a>
             @if($page->complete)
                 <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
-            @endif
-            @if($page->current)
-                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
             @endif
         </li>
     @endforeach
