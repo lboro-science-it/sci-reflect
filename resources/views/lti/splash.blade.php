9f5ba00dd35e41db034addee6594c0d916027a2d
scslingsby <scslingsby@gmail.com>
2017-07-11 14:10:23 +0100

    Add placeholders for cookie handling

--- resources/views/lti/splash.blade.php
+++ resources/views/lti/splash.blade.php
@@ -16,7 +16,8 @@
     </p>
 @endisset
 
-Splash screen to ensure the user creates a cookie / initiates session.
+Splash screen to ensure the user creates a cookie / initiates session.<br><br>
+** insert control for cookie-less mode + instructions for setting sci-el.lboro.ac.uk as a trusted site **
 <form action="{{ url('launch') }}" method="POST">
     @foreach($params as $key => $value)
         <input type="hidden" name="{{ $key }}" value="{{ $value }}">
