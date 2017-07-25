324e179b9cfc786af33a9729f051b96fbffec329
scslingsby <scslingsby@gmail.com>
2017-07-25 11:14:59 +0100

    Add last round title to chart title

--- resources/views/activity/partials/_chart.blade.php
+++ resources/views/activity/partials/_chart.blade.php
@@ -1,7 +1,11 @@
 @isset($activityData->chartData)
     <div class="panel panel-default">
         <div class="panel-body" style="padding: 20px;">
-            <h3>Your skills</h3>
+            @if($activityData->rounds->completed->count() > 0)
+                <h3>Your skills after {{ $activityData->rounds->completed->last()->title }}</h3>
+            @else
+                <h3>Your skills</h3>
+            @endif
             @include('chart.partials._chart', ['chartData' => $activityData->chartData])
         </div>
     </div>
