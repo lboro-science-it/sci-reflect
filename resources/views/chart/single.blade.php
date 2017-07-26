4ef3cda7a03cae95e9913d0254480a916d22a6bc
scslingsby <scslingsby@gmail.com>
2017-07-26 15:31:27 +0100

    Add category panels around skills

--- resources/views/chart/single.blade.php
+++ resources/views/chart/single.blade.php
@@ -10,24 +10,29 @@
 
 <div class="row">
     <div class="col-md-5 col-md-offset-2">
+
+    @foreach($categories as $category)
         <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3>{{ $category->name }}</h3>
+            </div>
             <div class="panel-body">
-                <h3>Your skills</h3>
-                @foreach($categories as $category)
-                    <h4>{{ $category->name }}</h4>
-                    @foreach($category->skills as $skill)
-                    <div class="row">
-                        @include('skills.partials._horizontal', ['skill' => $skill, 'category' => $category, 'improve' => true])
-                    </div>
-                    @endforeach
+                @foreach($category->skills as $skill)
+                <div class="row">
+                    @include('skills.partials._horizontal', ['skill' => $skill, 'category' => $category, 'improve' => true])
+                </div>
                 @endforeach
             </div>
-        </div>
+        </div>                    
+    @endforeach
+
     </div>
     <div class="col-md-3">
         <div class="panel panel-default">
-            <div class="panel-body" style="padding: 20px;">
+            <div class="panel-heading">
                 <h3>Your skills after {{ $round->title }}</h3>
+            </div>
+            <div class="panel-body" style="padding: 20px;">
                 @include('chart.partials._chart', ['chartData' => $chartData])
             </div>
         </div>
