1f7d0ad4d27f45598aed61372eb029b745c52c21
scslingsby <scslingsby@gmail.com>
2017-07-27 10:01:31 +0100

    Move format partial to main activity space

--- app/Providers/ComposerServiceProvider.php
+++ app/Providers/ComposerServiceProvider.php
@@ -14,13 +14,16 @@ class ComposerServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // make $activity available to all views
         View::composer(
             '*', 'App\Http\ViewComposers\ActivityComposer'
         );
 
+        // make list of formats available to the format select drop down
         View::composer(
             'activity.partials._format', 'App\Http\ViewComposers\ActivityFormatComposer'
         );
+
 
         View::composer(
             ['activity.staff.design', 'activity.staff.dashboard'],
