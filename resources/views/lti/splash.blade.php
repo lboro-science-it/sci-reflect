2b91a793ea66de527470351b892363d6444b437d
scslingsby <scslingsby@gmail.com>
2017-08-01 11:32:32 +0100

    Add scaffold for splash screen design

--- resources/views/lti/splash.blade.php
+++ resources/views/lti/splash.blade.php
@@ -1,28 +1,56 @@
 @extends('layouts.app')
 
+@section('navbar-class')
+
+@endsection
+
 @section('title')
 
 @isset($params['custom_title'])
-
+{{ $params['custom_title'] }}
 @endisset
 
 @endsection
 
 @section('content')
 
-@isset($params['custom_welcome'])
-    <p>
-    {{ $params['custom_welcome'] }}
-    </p>
-@endisset
+{{-- parent is full height --}}
 
-Splash screen to ensure the user creates a cookie / initiates session.<br><br>
-** insert control for cookie-less mode + instructions for setting sci-el.lboro.ac.uk as a trusted site **
-<form action="{{ url('launch') }}" method="POST">
-    @foreach($params as $key => $value)
-        <input type="hidden" name="{{ $key }}" value="{{ $value }}">
-    @endforeach
-    <input type="submit">
-</form>
+<div class="splash-container">
+
+    <div class="splash-centre">
+        @isset($params['custom_welcome'])
+            <div class="row">
+                <div class="col-md-10 col-md-offset-2">
+                    {{ $params['custom_welcome'] }}
+                </div>
+            </div>
+        @endisset
+        <div class="row">
+            <div class="col-md-3 col-md-offset-2">
+                ** insert image **
+            </div>
+            <div class="col-md-5 text-center">
+                <h1>Some tagline about reflecting. Word.</h1>
+                <form action="{{ url('launch') }}" method="POST">
+                    @foreach($params as $key => $value)
+                        <input type="hidden" name="{{ $key }}" value="{{ $value }}">
+                    @endforeach
+                    <input type="submit" class="btn btn-info btn-lg">
+                </form>
+            </div>
+        </div>
+
+
+        <div class="row">
+            <div class="col-md-10 col-md-offset-2">
+                ** insert instructions for cookies - setting sci-el.lboro.ac.uk as trusted **
+            </div>
+        </div>
+
+    </div>
+
+</div>
+
 
 @endsection
