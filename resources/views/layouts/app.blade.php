e53102d1319116ac82a988fd2cf7427a36d43926
scslingsby <scslingsby@gmail.com>
2017-08-01 09:41:23 +0100

    Stop navbar breaking onto two lines in responsive mode

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,60 +8,46 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'sciReflect') }}</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
+        <nav class="navbar navbar-default navbar-static-top @yield('navbar-header-class')">
             <div class="container-fluid">
                 <div class="navbar-header">
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
                     @if($activity)
                         <a class="navbar-brand" href="{{ url($homeUrl) }}">
-                            {{ config('app.name', 'Laravel') }}
+                            {{ config('app.name', 'sciReflect') }}
                         </a>
                     @endif
                 </div>
 
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <h4 class="navbar-text">@yield('title')</h4>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <h4 class="navbar-text">@yield('title')</h4>
+                    @if (Auth::check() && Auth::user()->role == 'staff')
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
 
-                        @if (Auth::check() && Auth::user()->role == 'staff')
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li>
+                                    Edit Activity
+                                </li>
+                                <li>
+                                    Rate students
+                                </li>
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
 
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        Edit Activity
-                                    </li>
-                                    <li>
-                                        Rate students
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
             </div>
         </nav>
         <div class="container-fluid">
