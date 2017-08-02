e872d686bc079abbbdebcf5b7c1d1e91f5df4d57
scslingsby <scslingsby@gmail.com>
2017-08-02 15:35:30 +0100

    Only show list if items to show

--- resources/views/activity/staff/partials/_studentlist.blade.php
+++ resources/views/activity/staff/partials/_studentlist.blade.php
@@ -1,11 +1,13 @@
-<div class="row">
-    <div class="col-md-10 col-md-offset-1">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3>Students</h3>
-            </div>
-            <div class="panel-body">
-                @if($students->count())
+@if($students->count())
+
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3>Students</h3>
+                </div>
+                <div class="panel-body">
+
                     <table class="table">
                         <thead>
                             <tr>
@@ -71,8 +73,10 @@
                             @endforeach
                         </tbody>
                     </table>
-                @endif
+
+                </div>
             </div>
         </div>
     </div>
-</div>
+
+@endif
--- resources/views/groups/partials/_list.blade.php
+++ resources/views/groups/partials/_list.blade.php
@@ -1,11 +1,11 @@
-<div class="row">
-    <div class="col-md-6 col-md-offset-3">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3>Groups</h3>
-            </div>
-            <div class="panel-body">
-                @isset($groups)
+@if($groups->count() > 0)
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3>Groups</h3>
+                </div>
+                <div class="panel-body">
                     <table class="table">
                         <thead>
                             <tr>
@@ -26,8 +26,8 @@
                             @endforeach
                         </tbody>
                     </table>
-                @endisset
+                </div>
             </div>
         </div>
     </div>
-</div>
+@endif
