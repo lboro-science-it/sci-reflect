37bd03248eafbc3cb7516712467a12f7101c1897
scslingsby <scslingsby@gmail.com>
2017-08-09 11:37:26 +0100

    Add route prefix and tidy up routes in general

--- routes/web.php
+++ routes/web.php
@@ -14,42 +14,59 @@
 // lti launch route
 Route::post('launch', 'LtiController@launch');
 
-Route::group(['middleware' => ['staff']], function() {
-    // staff activity routes
-    Route::get('a/{activity}', 'ActivityController@show');
-    Route::put('a/{activity}', 'ActivityController@create');
-    Route::put('a/{activity}/close', 'ActivityController@close');
-    Route::put('a/{activity}/open', 'ActivityController@open');
+/** 
+ * Activity routes all start with a/{activity}. In all of these routes, Route
+ * model binding is used to test the Authed user's authentication in the activity
+ * i.e. that they have access, whether they are staff or student.
+ * So every request after launch causes DB query to Auth::user()->activities,
+ * which gets the activity with pivot table containing role and other data.
+ */
+Route::prefix('a/{activity}')->group(function() {
+    /**
+     * Staff routes, middleware prevents these being accessed unless user is
+     * authed, has a relationship to the {activity}, and has role 'staff'
+     */
+    Route::group(['middleware' => ['staff']], function() {
+        // staff activity routes
+        Route::get('/', 'ActivityController@show');
+        Route::put('/', 'ActivityController@create');
+        Route::put('close', 'ActivityController@close');
+        Route::put('open', 'ActivityController@open');
 
-    Route::get('a/{activity}/add-users', 'UserController@create');
-    Route::post('a/{activity}/add-users', 'UserController@store');
+        Route::get('add-users', 'UserController@create');
+        Route::post('add-users', 'UserController@store');
 
-    // Group routes
-    Route::get('a/{activity}/groups', 'GroupController@index');
-    Route::delete('a/{activity}/groups/{groupId}', 'GroupController@delete');
-    Route::post('a/{activity}/groups/bulk', 'GroupController@bulk');
-    Route::post('a/{activity}/groups/batch', 'GroupController@batch');
-    Route::put('a/{activity}/groups/{groupId}', 'GroupController@update');
+        // Group routes
+        Route::get('groups', 'GroupController@index');
+        Route::delete('groups/{groupId}', 'GroupController@delete');
+        Route::post('groups/bulk', 'GroupController@bulk');
+        Route::post('groups/batch', 'GroupController@batch');
+        Route::put('groups/{groupId}', 'GroupController@update');
 
-    // Student User routes
-    Route::put('a/{activity}/student/{userId}/group', 'StudentUserController@updateGroup');
-    // bulk add students to group
-    Route::put('a/{activity}/group/{groupId}', 'StudentUserController@bulkGroup');
+        // Student User routes
+        Route::put('student/{userId}/group', 'StudentUserController@updateGroup');
+        // bulk add students to group
+        Route::put('group/{groupId}', 'StudentUserController@bulkGroup');
 
-    Route::get('a/{activity}/setup', 'ActivityController@showSetup');
-});
+        Route::get('setup', 'ActivityController@showSetup');
+    });
 
-Route::group(['middleware' => ['student']], function() {
-    // student linear routes
-    Route::get('a/{activity}/linear', 'LinearController@dashboard');
-    Route::post('a/{activity}/linear/r/{round}/p/{roundPage}', 'LinearController@page');
+    /**
+     * Student routes, middleware prevents these being accessed unless used is
+     * authed, has a relationship to the {activity}, and role 'student'
+     */
+    Route::group(['middleware' => ['student']], function() {
+        // student linear routes
+        Route::get('linear', 'LinearController@dashboard');
+        Route::post('linear/r/{round}/p/{roundPage}', 'LinearController@page');
 
-    // student nonlinear routes
-    Route::get('a/{activity}/nonlinear', 'NonLinearController@dashboard');
-    Route::post('a/{activity}/nonlinear/r/{round}/c/{category}/p/{categoryPage}', 'NonLinearController@page');
+        // student nonlinear routes
+        Route::get('nonlinear', 'NonLinearController@dashboard');
+        Route::post('nonlinear/r/{round}/c/{category}/p/{categoryPage}', 'NonLinearController@page');
 
-    // student chart routes
-    Route::get('a/{activity}/student/r/{round}/chart/{scope?}', 'StudentChartController@show');
+        // student chart routes
+        Route::get('student/r/{round}/chart/{scope?}', 'StudentChartController@show');
+    });
 });
 
 // security routes
