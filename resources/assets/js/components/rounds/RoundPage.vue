25e36b1cd4938ef1b1619525f099480ac6e79cb4
scslingsby <scslingsby@gmail.com>
2017-08-23 17:35:45 +0100

    Autofocus input on click of edit button

--- resources/assets/js/components/rounds/RoundPage.vue
+++ resources/assets/js/components/rounds/RoundPage.vue
@@ -5,10 +5,13 @@
          <span v-show="!editTitle">
              {{ editPage.page_number }}: {{ editPage.title }}
          </span>
-         <input type="text" v-model="editPage.title" v-show="editTitle">
+         <input type="text" :id="editPage.id"
+                            v-model="editPage.title" 
+                            v-show="editTitle"
+                            v-on:keyup.enter="saveTitle(index)">
 
          <div class="pull-right">
-             <button v-on:click="editTitle = true" v-show="!editTitle">edit</button>
+             <button v-on:click="activateEditing(index)" v-show="!editTitle">edit</button>
              <button v-on:click="saveTitle(index)" v-show="editTitle">save</button>
              <button v-on:click="cancelEditTitle(index)" v-show="editTitle">cancel</button>
              <button v-on:click="deletePage(index)" :class="{ disabled: canDelete }" v-show="!editTitle">delete</button>
@@ -33,14 +36,15 @@
 
         methods: {
             activateEditing(index) {
-                console.log('activateEditing for ' + index);
-                this.editPages[index].editing = true;
-                console.log(this.editPages[index]);
+                this.editTitle = true;
+                let editInput = document.getElementById(this.editPage.id);
+                Vue.nextTick(function() { 
+                    editInput.focus();
+                });
             },
 
+            // reset title to original page title and turn off editing
             cancelEditTitle(index) {
-                // todo: reset to original title... even though we don't have it here.
-                // but if we get pages we can get it
                 this.editTitle = false;
                 this.editPage.title = JSON.parse(JSON.stringify(this.originalPage.title));
             },
@@ -51,10 +55,11 @@
                 }
             },
 
-
             saveTitle(index) {
+                // todo: post new title to server and update original page title from that
+                this.originalPage.title = JSON.parse(JSON.stringify(this.editPage.title));
+                this.editTitle = false;
                 console.log('here we would save the page title');
-                console.log('so we need to know the page being saved here');
             }            
         }
     }
